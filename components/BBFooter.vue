6f6c2973af13d4ad6b4f0a3db9a0966578d5e1d3
Caalb <carlosmunizfjunior@gmail.com>
2023-05-06 14:02:07 -0300

    feat(BBFooter): cria footer

---
+++ components/BBFooter.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-footer
+		dark
+		padless
+		app
+		inset
+    
+	>
+		<v-card
+			class="flex"
+			flat
+			tile
+			color="primary"
+			:elevation="10"
+		>
+
+			<v-card-text class="py-2 white--text text-center">
+				{{ new Date().getFullYear() }} — <strong>Bibliobook</strong>
+			</v-card-text>
+		</v-card>
+	</v-footer>
+</template>
+
+<script>
+  export default {
+    name: 'BBFooter',
+  };
+</script>
+
+
