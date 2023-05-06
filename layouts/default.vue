16415cf30a6b2c2d02d955658ae193efafd5e7d9
Caalb <carlosmunizfjunior@gmail.com>
2023-05-06 14:02:57 -0300

    feat(layouts/default): cria um layout default para o app

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,22 @@
 <template>
-	<Nuxt />
+	<v-app>
+		<Navbar />
+		<v-content class="ma-4">
+			<Nuxt />
+		</v-content>
+		<BBFooter />
+	</v-app>
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue';
+import BBFooter from '../components/BBFooter.vue';
+
 export default {
-  name: 'DefaultLayout',
+    name: "DefaultLayout",
+    components: { 
+      Navbar,
+      BBFooter,
+     },
 };
 </script>
