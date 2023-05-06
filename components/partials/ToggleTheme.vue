5ba39bebfd3c23ce81675a8d22ae770a86b7b5be
Caalb <carlosmunizfjunior@gmail.com>
2023-05-06 14:02:30 -0300

    feat(ToggleTheme): cria componente para alteraçao de tema

---
+++ components/partials/ToggleTheme.vue
@@ -0,0 +1,17 @@
+<template>
+	<v-btn icon @click="toggleTheme">
+		<v-icon>
+			{{ $vuetify.theme.isDark ? 'mdi-white-balance-sunny' : 'mdi-weather-night' }}
+		</v-icon>
+	</v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
+</script>
