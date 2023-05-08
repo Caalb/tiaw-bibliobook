84cddb70a1c8e1b071e7f56da7adaf980b3bfaf3
Caalb <carlosmunizfjunior@gmail.com>
2023-05-08 20:05:34 -0300

    feat(books/index): cria tela de meus livros favoritos

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,11 +1,53 @@
 <template>
-	<div>
-		<h3>Meus Livros</h3>
-	</div>
+	<v-container>
+		<v-row>
+			<v-col cols="12">
+				<h1 class="text-center my-5">Meus livros favoritos</h1>
+			</v-col>
+		</v-row>
+
+		<v-row justify="center" class="my-5">
+			<v-col cols="12" sm="6" md="4" lg="3">
+				<v-text-field
+					label="Nome"
+					multiple
+					clearable
+					:color="getColorInput"
+				></v-text-field>
+			</v-col>
+
+			<v-col cols="12" sm="6" md="4" lg="3">
+				<v-text-field
+					label="Autor"
+					multiple
+					clearable
+					:color="getColorInput"
+				></v-text-field>
+			</v-col>
+		</v-row>
+
+		<v-row>
+			<v-col v-for="i in 7" :key="i" cols="12" sm="6" md="4" lg="3">
+				<BBMyBooksCard/>
+			</v-col>
+		</v-row>
+	</v-container>
+
 </template>
 
 <script>
+import BBMyBooksCard from '../../components/partials/BBMyBooksCard.vue';
+
   export default {
     name: 'BBBooks',
+    components: {
+      BBMyBooksCard,
+    },
+
+    computed: {
+      getColorInput() {
+        return this.$vuetify.theme.isDark ? 'white' : 'dark';
+      },
+    },
   };
 </script>
