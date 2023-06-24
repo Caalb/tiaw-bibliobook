869ae6f12fc818420d876ca6de2c054bb37175d1
Caalb <carlosmunizfjunior@gmail.com>
2023-06-24 20:07:14 -0300

    feat(books/index): adiciona busca no meus livros favoritos

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -7,28 +7,35 @@
 		</v-row>
 
 		<v-row justify="center" class="my-5">
-			<v-col cols="12" sm="6" md="4" lg="3">
+			<v-col cols="12" sm="6" md="5" lg="4">
 				<v-text-field
+					v-model="search_input"
 					label="Nome"
 					multiple
 					clearable
 					:color="getColorInput"
-				></v-text-field>
-			</v-col>
-
-			<v-col cols="12" sm="6" md="4" lg="3">
-				<v-text-field
-					label="Autor"
-					multiple
-					clearable
-					:color="getColorInput"
+					@change="searchBook"
 				></v-text-field>
 			</v-col>
 		</v-row>
 
 		<v-row>
-			<v-col v-for="book in getFavoritesBooks" :key="book.id" cols="12" sm="6" md="4" lg="3">
+			<v-col
+				v-for="book in favorites_book"
+				:key="book.id"
+				cols="12"
+				sm="6"
+				md="4"
+				lg="3"
+			>
 				<BBMyBooksCard :book="book"/>
+			</v-col>
+
+			<v-col
+				v-if="getRenderEmptyMessage"
+				class="text-center mt-5"
+			>
+				{{ getEmptyMessage }}
 			</v-col>
 		</v-row>
 	</v-container>
@@ -46,13 +53,55 @@ export default {
     BBMyBooksCard,
   },
 
+  data() {
+    return {
+      search_input: '',
+      favorites_book: [],
+    };
+  },
+
   computed: {
     getColorInput() {
       return this.$vuetify.theme.isDark ? 'white' : 'dark';
     },
 
-    getFavoritesBooks() {
-      return getFavoritesBooks();
+    getEmptyMessage() {
+      const not_found = 'Nenhum livro correspondente foi encontrado.';
+      const dont_have_favorites_books = 'Você não possui nenhum livro favorito no momento. Para adicionar livros aos favoritos, faça uma busca e clique no ícone de coração.';
+
+      return getFavoritesBooks().length ? not_found : dont_have_favorites_books;
+    },
+
+    getRenderEmptyMessage() {
+      return this.favorites_book.length === 0;
+    },
+  },
+
+  mounted() {
+    this.favorites_book = getFavoritesBooks();
+  },
+
+  watch: {
+    search_input(value) {
+      if (value) {
+        return this.searchBook();
+      }
+
+
+      this.favorites_book = getFavoritesBooks();
+    },
+  },
+
+  methods: {
+    searchBook() {
+      const filtred_books = getFavoritesBooks().filter((book) => {
+        const { volumeInfo: { title } = {} } = book;
+
+        return title.toLowerCase().includes(this.search_input.toLowerCase());
+      });
+
+
+      this.favorites_book = filtred_books;
     },
   },
 };
