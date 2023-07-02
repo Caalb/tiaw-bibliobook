cd2555aff9d909abe13ae995bbdc0de8541527d1
Caalb <carlosmunizfjunior@gmail.com>
2023-07-01 21:49:50 -0300

    feat(actions/fetchBooks): adiciona paginaçao e filtragem de livros gratuitos

--- store/book/actions.js
+++ store/book/actions.js
@@ -1,6 +1,16 @@
 export default {
-  async fetchBooks({ commit }, { book_data }) {
-    const response = await this.$api.$get(`/volumes?q=${book_data}`);
+  async fetchBooks({ commit }, { book_data, free_books, start_index = 0 }) {
+    const params = new URLSearchParams({
+      q: book_data,
+      startIndex: start_index,
+      maxResults: 10,
+    });
+
+    if (free_books) {
+      params.append('filter', 'free-ebooks');
+    }
+
+    const response = await this.$api.$get('/volumes', { params });
 
     commit('setBooks', response);
   },
